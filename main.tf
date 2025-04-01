76bf243ff59cd6c403d3a495b3f1d459612bbb60
aaron lim <aaron.limse@hotmail.com>
2025-04-01 21:13:44 +0800

    tf backend s3

---
+++ main.tf
@@ -0,0 +1,15 @@
+
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce9-tfstate"
+    key    = "aalimsee-ce9-M3.1-github-workflow.tfstate"
+    # Replace the value of key to <your>.tfstate, eg. terraform-ex-ec2-<NAME>.tfstate
+    region = "us-east-1"
+  }
+}
